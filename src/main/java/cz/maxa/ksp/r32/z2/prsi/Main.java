d7b5f1f67b8c9992ddca3e93b58de388a8c69819
Ondřej Maxa <majframe@majksa.cz>
2019-11-27 11:14:35 +0100

    Parse input

--- src/main/java/cz/maxa/ksp/r32/z2/prsi/Main.java
+++ src/main/java/cz/maxa/ksp/r32/z2/prsi/Main.java
@@ -1,8 +1,12 @@
 package cz.maxa.ksp.r32.z2.prsi;
 
 public class Main {
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        int width = Integer.parseInt(SCANNER.nextLine());
+        List<Integer> heights = Arrays.stream(
+                SCANNER.nextLine().split(" ")
+        ).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
     }
 }
